aab53e38f5113d4f1d504b2724f145e3e626321c
stefan.busemann <stefan.busemann@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-08-09 11:55:03 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dam_frontend/branches@11046 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ 0.1.x/ext_localconf.php
@@ -0,0 +1,22 @@
+<?php
+if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+
+  ## Extending TypoScript from static template uid=43 to set up userdefined tag:
+t3lib_extMgm::addTypoScript($_EXTKEY,'editorcfg','
+	tt_content.CSS_editor.ch.tx_damfrontend_pi1 = < plugin.tx_damfrontend_pi1.CSS_editor
+',43);
+
+// Adding a static file to the script
+t3lib_extMgm::addStaticFile($_EXTKEY,"static/","DAM Frontend Static Template");
+
+t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_damfrontend_pi1.php','_pi1','list_type',1);
+
+
+
+  ## Extending TypoScript from static template uid=43 to set up userdefined tag:
+t3lib_extMgm::addTypoScript($_EXTKEY,'editorcfg','
+    tt_content.CSS_editor.ch.tx_damfrontend_pi2 = < plugin.tx_damfrontend_pi2.CSS_editor
+',43);
+
+t3lib_extMgm::addPItoST43($_EXTKEY,'pi2/class.tx_damfrontend_pi2.php','_pi2','list_type',1);
+?>
