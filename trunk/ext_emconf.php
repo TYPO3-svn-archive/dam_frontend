f0993e99d841bb28e33578e4a8ef3745f55f318d
stefan.busemann <stefan.busemann@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-09-14 08:53:56 +0000

    0.2.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dam_frontend/trunk@11941 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ trunk/ext_emconf.php
@@ -0,0 +1,54 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "dam_frontend"
+#
+# Auto generated 14-09-2008 10:50
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'DAM Frontend',
+	'description' => 'Shows the dam category tree in frontend, so users can select files by combining dam categories. Files are delivered by a pushfile, so that the fileadmin directory can be secured by a .htaccess file. A second plugin shows selected (by an author) dam files. Please read the manual for more informations.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '0.2.0',
+	'dependencies' => 'dam,fileupload,dam_index',
+	'conflicts' => 'realurl',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Martin Baum, Stefan Busemann',
+	'author_email' => 'typo3@in2form.com',
+	'author_company' => 'in2form.com',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'dam' => '1.0.13-1.0.99',
+			'php' => '4.0.0-0.0.0',
+			'typo3' => '4.0.0-0.0.0',
+			'fileupload' => '1.1.0-0.0.0',
+			'dam_index' => '1.0.4-0.0.0',
+		),
+		'conflicts' => array(
+			'realurl' => '0.0.0-0.0.0',
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:47:{s:9:"ChangeLog";s:4:"2e2a";s:10:"README.txt";s:4:"447a";s:12:"ext_icon.gif";s:4:"69ef";s:17:"ext_localconf.php";s:4:"89fc";s:15:"ext_php_api.dat";s:4:"9bb6";s:14:"ext_tables.php";s:4:"eca0";s:14:"ext_tables.sql";s:4:"92e4";s:15:"flexform_ds.xml";s:4:"1b7e";s:24:"flexform_ds_disabled.xml";s:4:"8bd6";s:16:"locallang_db.xml";s:4:"5682";s:17:"locallang_tca.xml";s:4:"ed8b";s:12:"pushfile.php";s:4:"ebba";s:7:"tca.php";s:4:"422e";s:19:"tx_dam_flexFunc.php";s:4:"70c1";s:43:"DAL/class.tx_damfrontend_DAL_categories.php";s:4:"441a";s:42:"DAL/class.tx_damfrontend_DAL_documents.php";s:4:"7159";s:44:"DAL/class.tx_damfrontend_baseSessionData.php";s:4:"157a";s:36:"DAL/class.tx_damfrontend_catList.php";s:4:"3f16";s:36:"DAL/class.tx_damfrontend_docList.php";s:4:"f337";s:40:"DAL/class.tx_damfrontend_filterState.php";s:4:"c7c7";s:38:"DAL/class.tx_damfrontend_listState.php";s:4:"910b";s:14:"doc/manual.sxw";s:4:"3185";s:19:"doc/wizard_form.dat";s:4:"87c9";s:20:"doc/wizard_form.html";s:4:"26f6";s:45:"frontend/class.tx_damfrontend_catTreeView.php";s:4:"a0bc";s:52:"frontend/class.tx_damfrontend_categorisationTree.php";s:4:"92cc";s:43:"frontend/class.tx_damfrontend_rendering.php";s:4:"e10f";s:32:"pi1/class.tx_damfrontend_pi1.php";s:4:"be66";s:17:"pi1/locallang.xml";s:4:"870c";s:13:"pi1/style.css";s:4:"842c";s:17:"pi1/template.html";s:4:"f8c6";s:24:"pi1/static/editorcfg.txt";s:4:"c15d";s:32:"pi2/class.tx_damfrontend_pi2.php";s:4:"1a5b";s:17:"pi2/locallang.xml";s:4:"c5e0";s:24:"pi2/static/editorcfg.txt";s:4:"375a";s:20:"pi2/static/setup.txt";s:4:"bc30";s:11:"res/ddl.css";s:4:"1f06";s:13:"res/style.css";s:4:"0eb9";s:18:"res/tmpl_list.html";s:4:"e599";s:31:"res/ico/application_default.png";s:4:"b307";s:27:"res/ico/application_pdf.png";s:4:"8b7e";s:19:"res/ico/default.png";s:4:"ce6f";s:18:"res/ico/hidden.png";s:4:"5894";s:25:"res/ico/image_default.png";s:4:"c549";s:24:"res/ico/text_default.png";s:4:"cf7d";s:20:"static/constants.txt";s:4:"04a8";s:16:"static/setup.txt";s:4:"53a8";}',
+	'suggests' => array(
+	),
+);
+
+?>
---
+++ trunk/pi1/class.tx_damfrontend_pi1.php
@@ -0,0 +1,541 @@
+<?php
+
+require_once(PATH_tslib.'class.tslib_pibase.php');
+
+// references to the DAL
+require_once(t3lib_extMgm::extPath('dam_frontend').'/DAL/class.tx_damfrontend_DAL_documents.php');
+require_once(t3lib_extMgm::extPath('dam_frontend').'/DAL/class.tx_damfrontend_DAL_categories.php');
+require_once(t3lib_extMgm::extPath('dam_frontend').'/DAL/class.tx_damfrontend_catList.php');
+require_once(t3lib_extMgm::extPath('dam_frontend').'/DAL/class.tx_damfrontend_filterState.php');
+require_once(t3lib_extMgm::extPath('dam_frontend').'/DAL/class.tx_damfrontend_listState.php');
+require_once(t3lib_extMgm::extPath('dam_frontend').'/frontend/class.tx_damfrontend_catTreeView.php');
+require_once(t3lib_extMgm::extPath('dam_frontend').'/frontend/class.tx_damfrontend_rendering.php');
+
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2006-2007 BUS Netzwerk (typo3@in2form.com)
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*  A copy is found in the textfile GPL.txt and important notices to the license
+*  from the author is found in LICENSE.txt distributed with these scripts.
+*
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ *
+ * class.tx_damfrontend_pi1.php
+ *
+ * Plugin 'DAM Frontend' for the 'dam_frontend' extension.
+ *
+ * @package typo3
+ * @subpackage tx_dam_frontend
+ * @author Martin Baum <typo3@in2form.com>
+ *
+ * @todo add stdWrap functions
+ * Some scripts that use this class:	--
+ * Depends on:		--
+ */
+/**
+ * [CLASS/FUNCTION INDEX of SCRIPT]
+ *
+ *
+ *
+ *   79: class tx_damfrontend_pi1 extends tslib_pibase
+ *  101:     function init()
+ *  120:     function initFilter()
+ *  156:     function initList()
+ *  193:     function initUpload()
+ *  202:     function convertPiVars()
+ *  243:     function loadFlexForm()
+ *  264:     function main($content,$conf)
+ *  346:     function catTree()
+ *  359:     function getTree($mount= '')
+ *  378:     function fileList()
+ *  424:     function filterView()
+ *  437:     function catSelection()
+ *  466:     function singleView()
+ *  488:     function filterList()
+ *  516:     function uploadForm()
+ *
+ * TOTAL FUNCTIONS: 15
+ * (This index is automatically created/updated by the extension "extdeveval")
+ *
+ */
+class tx_damfrontend_pi1 extends tslib_pibase {
+	var $prefixId = 'tx_damfrontend_pi1';		// Same as class name
+	var $scriptRelPath = 'pi1/class.tx_damfrontend_pi1.php';	// Path to this script relative to the extension dir.
+	var $extKey = 'dam_frontend';	// The extension key.
+	var $pi_checkCHash = TRUE;
+
+
+	// references to the DAL
+	var $docLogic; // handling of documents
+	var $catLogic; // handling of categories
+
+	// references to various helpers
+	var $catList; // stores the category selection in the session
+	var $renderer; // handeles the frontend rendering
+	var $filterState; // stores the current filter state in the session, provides synchronisation with the $internal['filter'] array
+	var $listState; // stores the current state of the list
+
+	/**
+	 * Inits this class and instanceates all nescessary classes
+	 *
+	 * @return	[void]		...
+	 */
+	function init() {
+		// instanciate the references to the DAL
+		$this->docLogic = t3lib_div::makeInstance('tx_damfrontend_DAL_documents');
+		$this->catLogic= t3lib_div::makeInstance('tx_damfrontend_DAL_categories');
+		$this->catList = t3lib_div::makeInstance('tx_damfrontend_catList');
+		$this->renderer = t3lib_div::makeInstance('tx_damfrontend_rendering');
+		$this->filterState = t3lib_div::makeInstance('tx_damfrontend_filterState');
+		$this->listState = t3lib_div::makeInstance('tx_damfrontend_listState');
+		$this->renderer->setFileRef($this->conf['templateFile']);
+		$this->renderer->piVars = $this->piVars;
+		$this->renderer->conf = $this->conf;
+		$this->renderer->cObj = $this->cObj;
+	}
+
+	/**
+	 * Receiving filter information from the pluging view "filter_list"
+	 *
+	 * @return	[void]		...
+	 */
+	function initFilter() {
+		//variables for setting filters for the current category selection
+ 		$this->internal['filter']['from_day'] = intval(t3lib_div::_GP('von_tag'));
+ 		$this->internal['filter']['from_month'] =intval(t3lib_div::_GP('von_monat'));
+ 		$this->internal['filter']['from_year'] = intval(t3lib_div::_GP('von_jahr'));
+
+ 		$this->internal['filter']['to_day'] = (int)t3lib_div::_GP('bis_tag');
+ 		$this->internal['filter']['to_month'] = intval(t3lib_div::_GP('bis_monat'));
+ 		$this->internal['filter']['to_year'] = intval(t3lib_div::_GP('bis_jahr'));
+
+ 		// clear all 0 - values - now they are not shown in the frontend form
+ 		foreach ($this->internal['filter'] as $key => $value) {
+ 			if ($value == '0') $this->internal['filter'][$key] = '';
+ 		}
+
+ 		$this->internal['filter']['filetype'] = strip_tags(t3lib_div::_GP('filetype'));
+		$this->internal['filter']['searchword'] = strip_tags(t3lib_div::_GP('searchword'));
+
+		if (t3lib_div::_GP('resetFilter')) $this->filterState->resetFilter();
+		if (!count($this->filterState->getFilterFromSession())) {
+			$emptyArray = $this->internal['filter'];
+			foreach ($emptyArray as $key => $value) $emptyArray[$key] = ' ';
+			$this->filterState->setFilter($emptyArray);
+		}
+		if (t3lib_div::_GP('setFilter')) {
+			$this->filterState->setFilter($this->internal['filter']);
+		}
+		$this->internal['filter'] = $this->filterState->getFilterFromSession();
+		$this->docLogic->setFilter($this->internal['filter']);
+	}
+
+	/**
+	 * this function is preparing the listview
+	 *
+	 * @return	[void]		...
+	 */
+	function initList() {
+		// setting internal values for pagebrowsing from the incoming request
+ 		if (t3lib_div::_GP('setListLength')) {
+			$this->internal['list']['listLength'] = t3lib_div::_GP('listLength') != null ? intval(t3lib_div::_GP('listLength')) : 10;
+ 		}
+
+		$this->internal['list']['pointer'] =  $this->piVars['pointer'] != null ? intval($this->piVars['pointer']) : 0;
+		// setting the internal values for sorting
+		foreach ($this->piVars as $postvar => $postvalue) {
+ 			// clearing SQL Injection
+ 			if ($postvalue == 'DESC' || $postvalue == 'ASC') {
+ 				if (substr($postvar, 0, 5) == 'sort_') {
+ 					$this->internal['list']['sorting'] = strip_tags(substr($postvar, 5).' '.$postvalue);
+ 				}	
+ 			}
+ 			
+		}
+
+		$this->listState->syncListState($this->internal['list']);
+
+		if (!isset($this->internal['list']['listLength'])) $this->internal['list']['listLength'] = 10;
+
+		/*
+		 * if a filter criteria is changed, the pagebrowsing is reseted to the beginning value
+		 */
+		if (t3lib_div::_GP('setFilter') || !empty($this->internal['catPlus']) ||
+				!empty($this->internal['catPlus']) || !empty($this->internal['catMinus']) ||
+				!empty($this->internal['catEquals']) || !empty($this->internal['catPlus_Rec']) || !empty($this->internal['catMinus_Rec']))
+		{
+			$this->internal['list']['pointer'] = 0;
+		}
+		$this->internal['list']['limit'] = $this->internal['list']['pointer'].','. ($this->internal['list']['listLength']);
+		$this->listState->setListState($this->internal['list']);
+	}
+
+	/**
+	 * this function is providing the fileupload to the ter
+	 *
+	 * @return	[void]		...
+	 * @todo finish this functionality
+	 */
+	function initUpload() {
+		$fileContent = t3lib_div::_GP('fiel');
+	}
+
+	/**
+	 * reads the incoming piVars from the request and copies them to the internal array
+	 *
+	 * @return	void
+	 */
+	function convertPiVars() {
+		// variables for category selection
+		/*
+		$this->internal['catPlus'] = intval($this->piVars['catPlus']);
+		$this->internal['catMinus'] = intval($this->piVars['catMinus']);
+		$this->internal['catEquals'] = intval($this->piVars['catEquals']);
+		*/
+		$this->internal['catPlus'] = intval(t3lib_div::_GP('catPlus'));
+		$this->internal['catMinus'] = intval(t3lib_div::_GP('catMinus'));
+		$this->internal['catEquals'] = intval(t3lib_div::_GP('catEquals'));
+		$this->internal['catPlus_Rec'] = intval(t3lib_div::_GP('catPlus_Rec'));
+		$this->internal['catMinus_Rec'] = intval(t3lib_div::_GP('catMinus_Rec'));
+
+		// Aufruf der Einzelansicht
+		$this->internal['singleID'] = intval($this->piVars['showUid']);
+
+		// loading var for displaying a form for creation of a new filter state
+		$this->internal['newFilter'] = strip_tags(t3lib_div::_GP('newFilter'));
+
+		// getting the incoming treeID
+		$this->internal['incomingtreeID'] = intval(t3lib_div::_GP('treeID'));
+
+		// Selection Mode
+		$this->internal['slectionMode'] = intval($this->piVars['selectionMode']);
+
+		// values for searching
+		// Setting new values
+ 		if ($this->internal['viewID'] == 1  ) {
+			$this->initList();
+			$this->initFilter();
+ 		}
+		if ($this->internal['viewID'] == 5) {
+			$this->initFilter();
+		}
+ 	}
+
+	/**
+	 * loads the data from the flexform into the internal array
+	 *
+	 * @return	void
+	 */
+	function loadFlexForm() {
+		// getting values from flexform
+		$this->pi_initPIflexForm();
+		$flexform = $this->cObj->data['pi_flexform'];
+
+		$this->internal['viewID'] = intval($this->pi_getFFvalue($flexform, 'viewID'));
+//		debug($this->pi_getFFvalue($flexform, 'catMounts', sSelection));
+		$this->internal['catMounts'] = explode(',',$this->pi_getFFvalue($flexform, 'catMounts', 'sSelection'));
+
+		$this->internal['treeName'] = strip_tags($this->pi_getFFvalue($flexform, 'treeName', 'sSelection'));
+		$this->internal['treeID'] = $this->cObj->data['uid'];
+	}
+
+
+	/**
+	 * The main method of the PlugIn
+	 *
+	 * @param	string		$content: The PlugIn content
+	 * @param	array		$conf: The PlugIn configuration
+	 * @return	string		The html content that is displayed on the website
+	 */
+	function main($content,$conf)	{
+		$this->conf=$conf;
+		$this->pi_setPiVarDefaults();
+		$this->pi_loadLL();
+
+		// initialilisation and convertion of input paramters
+		// reading parameters from different sources
+		//check if a fe_user is logged in
+		$this->init();
+		$this->loadFlexForm();
+		$this->convertPiVars();
+
+		$this->filterState->filterTable = 'tx_damfrontend_filterStates';
+
+		// Executing some primary tests on the catgeory logic
+		// if ($this->internal['viewID'] == 2) $this->test();
+
+		// Processing and distribution of input data
+		// Mapping input parameters to actions
+		if ($this->internal['viewID'] == 2 && $this->internal['treeID'] == $this->internal['incomingtreeID'] ) {
+			if ($this->internal['catPlus']) {
+				// first check, if the user has access to the category
+				//if ($this->catLogic->checkCategoryAccess($GLOBALS['TSFE']->fe_user->user['uid'],$this->internal['catPlus'])) {
+					$this->catList->op_Plus($this->internal['catPlus'], $this->internal['incomingtreeID']);	
+				//}
+			}
+			else if ($this->internal['catMinus']) {	
+				$this->catList->op_Minus($this->internal['catMinus'], $this->internal['incomingtreeID']);
+			}
+			else if ($this->internal['catEquals']) {
+				$this->catList->op_Equals($this->internal['catEquals'], $this->internal['incomingtreeID']);
+			}
+			else if ($this->internal['catMinus_Rec']) {
+				$subs = $this->catLogic->getSubCategories($this->internal['catMinus_Rec']);
+				foreach ($subs as $sub) {
+					$this->catList->op_Minus($sub['uid'], $this->internal['incomingtreeID']);
+				}
+			}
+			else if ($this->internal['catPlus_Rec']) {
+				$subs = $this->catLogic->getSubCategories($this->internal['catPlus_Rec']);
+				foreach ($subs as $sub) {
+					$this->catList->op_Plus($sub['uid'], $this->internal['incomingtreeID']);
+				}
+			}
+//		debug($this->catList->getCatSelection());
+		}
+
+		// Mapping the ViewIds - selected in the Flexform to the content
+		// that shall be rendered
+		//debug($this->internal['viewID']);
+		switch ($this->internal['viewID']) {
+			case 1:
+				$content = $this->fileList();
+				break;
+			case 2:
+				$content = $this->catTree();
+				break;
+			case 3:
+				$content = $this->catSelection();
+				break;
+			case 4:
+				$content = $this->singleView();
+				break;
+			case 5:
+				$content = $this->filterView();
+				break;
+			case 6:
+				$content = $this->filterList();
+				break;
+			case 7:
+				$content = $this->uploadForm();
+			default:
+				$content = 'no view selected - nothing is displayed';
+				break;
+		}
+		// select the view to be created
+		return $this->pi_wrapInBaseClass($content);
+	}
+
+
+	/**
+	 * renderes an category tree
+	 *
+	 * @return	html		category tree - ready for display
+	 */
+	function catTree() {
+		##### Adding a treeview to the output
+		$tree = t3lib_div::makeInstance('tx_damfrontend_catTreeView');
+		$tree->init($this->internal['treeID']);
+		$tree->title = $this->internal['treeName'];
+		$selCats  = $this->catList->getCatSelection();
+		$tree->selectedCats = $selCats[$this->internal['treeID']];
+		if (is_array($this->internal['catMounts'])) $tree->MOUNTS = $this->internal['catMounts'];
+		return  '<div class="cattree" >'.$tree->getBrowsableTree().'</div>';
+	}
+
+
+	/*
+	function getTree($mount= '') {
+
+		$tree = t3lib_div::makeInstance('tx_damfrontend_catTreeView');
+		$tree->init();
+		$tree->selectedCats = $this->catList->getCatSelection();
+
+		if ($mount != '') $tree->MOUNTS;
+	}
+	*/
+
+
+	/**
+	 * fist this function gets the list of selected categories from the session vars
+	 * after that it retrievs a list of all documents from the doclogic
+	 * all documents are contained, which nat filtered and the user has
+	 * After that, the list is converted to an html view, rendered by the renderer instance
+	 *
+	 * @return	html		HTML - list of all selected documents
+	 */
+	function fileList() {
+		$cats = $this->catList->getCatSelection();
+		$hasCats = false; // true if any category has been selected yet
+		if (count($cats)) {
+			foreach($cats as $catList) {
+				if (count($catList)) $hasCats = true;
+			}
+		}
+
+		if ($hasCats ) {
+			/***************************
+			 *
+			 *    search and sorting values are transfered to the user
+			 *
+			 ***************************/
+
+			if (is_array($this->internal['filter'])) {
+				$this->internal['filterError'] = $this->docLogic->setFilter($this->internal['filter']);
+			}
+			$this->docLogic->orderBy = $this->internal['list']['sorting'];
+			$this->docLogic->limit = $this->internal['list']['limit'];
+			$this->docLogic->categories = $cats;
+			$this->docLogic->selectionMode = $this->internal['selectionMode'];
+			$files = $this->docLogic->getDocumentList();
+			if (is_array($files)) {
+				//get the html from the renderer
+				$rescount = $this->docLogic->resultCount;
+				$content = $this->renderer->renderFileList($files, $rescount, $this->internal['list']['pointer'], $this->internal['list']['listLength']);
+			}
+			else {
+				// render error
+				$content = $this->renderer->renderError('noDocInCat');
+			}
+		}
+		else {
+			//render error
+			$content = $this->renderer->renderError('noCatSelected');
+		}
+		return $content;
+	}
+
+	/**
+	 * Renders the filter view
+	 *
+	 * @return	[string]		$content html auf filterview
+	 */
+	function filterView() {
+		$content = $this->renderer->renderFilterView($this->internal['filter'], $this->internal['filterError']);
+		return $content;
+	}
+
+
+	/**
+	 * retrieves all selected catID's from the session and creates a list of category - records
+	 * by this list. After that, this list is given to the frontend - renderer to render a list of all
+	 * selected categories
+	 *
+	 * @return	html		rendered list, ready for display, of selected categories
+	 */
+	function catSelection() {
+		// getting the complete category selection
+		$selection = $this->catList->getCatSelection();
+		if (is_array($selection)) {
+			foreach($selection as $cat) {
+				$catList[] = $this->catLogic->getCategory($cat[0]);
+			}
+			if (is_array($catList))
+			{
+				$content = $this->renderer->renderCatSelection($catList);
+			}
+			else {
+				$content = $this->renderer->renderError('noCatSelected');
+			}
+
+		}
+		else {
+			$content = $this->renderer->renderError('noCatSelected');
+		}
+		return $content;
+	}
+
+
+	/**
+	 * controller function dor the rendering of the single view
+	 * the internal singleID must be set
+	 *
+	 * @return	html		rendered content
+	 */
+	function singleView() {
+		$singleID = intval($this->internal['singleID']);
+		if ($this->docLogic->checkAccess($singleID, 1)) {
+			if (intval($singleID) && $singleID != 0) {
+				$record = $this->docLogic->getDocument($singleID);
+				$content = $this->renderer->renderSingleView($record);
+	
+				if ($this->docLogic->checkAccess($singleID, 2)) {
+					$_SESSION['fileRef'] = $record['file_path'].$record['file_name'];
+				}
+				return $content;
+			}
+			else {
+				return $this->renderer->renderError('noSingleID');
+			}
+		}
+		else {
+			return $this->renderer->renderError('noDocAccess');
+		}
+		
+	}
+
+
+	/**
+	 * function is responsible for rendering of a list with all availible filters
+	 *
+	 * @return	string		html list of all availible filters
+	 */
+	function filterList() {
+		if (is_array($GLOBALS['TSFE']->fe_user->user)) {
+			$filterList = $this->filterState->getFilterList($GLOBALS['TSFE']->fe_user->user['uid']);
+			if (is_array($filterList) && count($filterList)) {
+				$content =$this->renderer->renderFilterList($filterList);
+			}
+			else {
+				$content = $this->renderer->renderError('noFilterStates');
+			}
+
+			if ($this->internal['newFilter'] != '') {
+				$content .= $this->renderer->renderFilterCreationForm();
+			}
+			else {
+				$content .= $this->renderer->renderFilterCreationLink();
+			}
+		}
+		else {
+			$content = $this->renderer->renderError('noUserLoggedIn');
+		}
+		return $content;
+	}
+
+	/**
+	 * Shows the upload form
+	 *
+	 * @return	[string]		$content the html code of the form
+	 */
+	function uploadForm() {
+		$content = $this->renderer->renderUploadForm();
+		return $content;
+	}
+
+}
+
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/dam_frontend/pi1/class.tx_damfrontend_pi1.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/dam_frontend/pi1/class.tx_damfrontend_pi1.php']);
+}
+
+?>
