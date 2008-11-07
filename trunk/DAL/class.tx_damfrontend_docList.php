1118d1cd68f3a05c0326f0e5290cd5d7559c81d2
martinbaum <martinbaum@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-11-07 23:57:34 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dam_frontend/tags/0.2.1@13928 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ trunk/DAL/class.tx_damfrontend_docList.php
@@ -0,0 +1,59 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2006-2008 in2form.com (typo3@in2form.com)
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*  A copy is found in the textfile GPL.txt and important notices to the license
+*  from the author is found in LICENSE.txt distributed with these scripts.
+*
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ *
+ * class.tx_damfrontend_docList.php
+ *
+ * What does it do?
+ *
+ * @package typo3
+ * @subpackage tx_dam_frontend
+ * @author Martin Baum <typo3@in2form.com>
+ * @todo this class is not used yet / delete it?
+ *
+ * Some scripts that use this class:	---
+ * Depends on:		--
+ */
+/**
+ * [CLASS/FUNCTION INDEX of SCRIPT]
+ *
+ *
+ *
+ *   52: class tx_damfrontend_docList
+ *
+ * TOTAL FUNCTIONS: 0
+ * (This index is automatically created/updated by the extension "extdeveval")
+ *
+ */
+class tx_damfrontend_docList {
+
+}
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/dam_frontend/DAL/class.tx_damfrontend_docList.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/dam_frontend/DAL/class.tx_damfrontend_docList.php']);
+}
+
+?>
