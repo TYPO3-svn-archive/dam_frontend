fad08c22c82389cdd821100bd8a1564dbcb238ca
stefan.busemann <stefan.busemann@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-05-25 12:00:27 +0000

    added pi2 to the content wizzard
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dam_frontend/trunk@20654 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -229,11 +229,11 @@ $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='la
 t3lib_extMgm::addPlugin(array('LLL:EXT:dam_frontend/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY.'_pi1'),'list_type');
 t3lib_extMgm::addStaticFile($_EXTKEY,"pi1/static/","DAM Frontend");
 t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY.'/flexform_ds.xml');
-
-if (TYPO3_MODE=='BE') $TBE_MODULES_EXT['﻿xMOD_db_new_content_el']['﻿addElClasses']['tx_damfrontend_pi1_wizicon'] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_damfrontend_pi1_wizicon.php';
-
+if (TYPO3_MODE=="BE") $TBE_MODULES_EXT["xMOD_db_new_content_el"]["addElClasses"]["tx_damfrontend_pi1_wizicon"] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_damfrontend_pi1_wizicon.php';
 t3lib_div::loadTCA('tt_content');
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi2']='layout,select_key';
 t3lib_extMgm::addPlugin(array('LLL:EXT:dam_frontend/locallang_db.xml:tt_content.list_type_pi2', $_EXTKEY.'_pi2'),'list_type');
 t3lib_extMgm::addStaticFile($_EXTKEY,"pi2/static/","DAM Frontend Filelist");
+if (TYPO3_MODE=="BE") $TBE_MODULES_EXT["xMOD_db_new_content_el"]["addElClasses"]["tx_damfrontend_pi2_wizicon"] = t3lib_extMgm::extPath($_EXTKEY).'pi2/class.tx_damfrontend_pi2_wizicon.php';
+
 ?>
