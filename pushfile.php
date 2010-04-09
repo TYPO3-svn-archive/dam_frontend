b7249d8d7c1199140a923b7348dbe5aae37c2551
stefan.busemann <stefan.busemann@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-04-09 07:52:11 +0000

    Feature #2808 improved templating
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dam_frontend/trunk@32074 735d13b6-9817-0410-8766-e36946ffe9aa

--- pushfile.php
+++ pushfile.php
@@ -150,8 +150,10 @@ if (is_array($post) && count($post) > 0) {
 		if (!$maildata['to']) die('<h1>Please set a recpient</h1><p>I\' sorry, without a recipient i can\'t send your mail</p>' );
 		
 		require_once(PATH_t3lib.'class.t3lib_htmlmail.php');
-		$mailTemplate = strip_tags($mailTemplate,'<table><tr><td><p>');          
-		$mailTemplate = str_replace('###MAIL_COMMENT###',strip_tags($maildata['body']),$mailTemplate);
+		$mailTemplate = str_replace( array("\r\n","\n","\r"), '<br>', $mailTemplate); // like nl2br() (sh 2010-03-28)
+		$mailTemplate = strip_tags($mailTemplate,'<table><tr><td><p><b><br>'); // allow b and br (sh 2010-03-28)
+		$maildata['htmlbody'] = str_replace( array("\r\n","\n","\r"), '<br>', $maildata['body'] ); // like nl2br() (sh 2010-03-28)
+		$mailTemplate = str_replace('###MAIL_COMMENT###',strip_tags($maildata['htmlbody'],'<br>'),$mailTemplate); // allow br (sh 2010-03-28)
 		$html_start='<html><head><title>Downloads</title></head><body>';
 		$html_end='</body></html>';
 		$htmlMail = t3lib_div::makeInstance('t3lib_htmlmail');
