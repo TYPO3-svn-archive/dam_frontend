01359b110fac3d7b8c7044ffcf382ee10481b878
maholtz <maholtz@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-05-15 09:30:26 +0000

    a new test, corrected author info
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dam_frontend/trunk@33205 735d13b6-9817-0410-8766-e36946ffe9aa

--- Tests/class.tx_damfrontend_testlib.php
+++ Tests/class.tx_damfrontend_testlib.php
@@ -2,7 +2,7 @@
 /***************************************************************
 *  Copyright notice
 *
-*  (c) 2009 
+*  (c) 2010 DAM Frontend Team 
 *  All rights reserved
 *
 *  This script is part of the TYPO3 project. The TYPO3 project is
