306ccd6ce2f003a5b49b1d7983d821935e5791ad
maholtz <maholtz@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-05-15 08:45:36 +0000

    first steps introducing unittests to dam_frontend
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dam_frontend/trunk@33204 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Tests/class.tx_damfrontend_testlib.php
@@ -0,0 +1,85 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2009 
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+require_once(PATH_tslib.'class.tslib_pibase.php');
+require_once(PATH_tslib.'class.tslib_content.php');
+
+
+// references to the DAL
+require_once(t3lib_extMgm::extPath('dam_frontend').'/DAL/class.tx_damfrontend_DAL_documents.php');
+require_once(t3lib_extMgm::extPath('dam_frontend').'/DAL/class.tx_damfrontend_DAL_categories.php');
+require_once(t3lib_extMgm::extPath('dam_frontend').'/DAL/class.tx_damfrontend_catList.php');
+require_once(t3lib_extMgm::extPath('dam_frontend').'/DAL/class.tx_damfrontend_filterState.php');
+require_once(t3lib_extMgm::extPath('dam_frontend') . '/DAL/class.tx_damfrontend_listState.php');
+require_once(t3lib_extMgm::extPath('dam_frontend') . '/frontend/class.tx_damfrontend_catTreeView.php');
+require_once(t3lib_extMgm::extPath('dam_frontend') . '/frontend/class.tx_damfrontend_catTreeViewAdvanced.php');
+require_once(t3lib_extMgm::extPath('dam_frontend') . '/frontend/class.tx_damfrontend_rendering.php');
+
+
+class tx_damfrontend_testlib extends tx_phpunit_testcase { 
+
+
+	/***************************************
+	 *
+	 *	 Fixtures
+	 *
+	 ***************************************/
+
+	/**
+	 * 
+	 */
+	/*
+	protected function addFixturePathToFilemount () {		
+		$filepath = $this->getFixtureFilename();
+		$filename = tx_dam::file_basename($filepath);
+		$testpath = tx_dam::file_dirname($filepath);
+		
+		$this->tempSave['fileadminDir'] = $GLOBALS['TYPO3_CONF_VARS']['BE']['fileadminDir'];
+		$GLOBALS['TYPO3_CONF_VARS']['BE']['fileadminDir'] = tx_dam::path_makeRelative($testpath);
+		
+		$GLOBALS['FILEMOUNTS']['__unittest'] = array(
+			'name' => (basename($testpath).'/'),
+			'path'=> $testpath,
+			'type' => '',
+		);
+	}
+	*/	
+	
+	
+	/**
+	 * 
+	 */
+	/*
+	protected function removeFixturePathFromFilemount () {
+		global $FILEMOUNTS;
+		
+		$GLOBALS['TYPO3_CONF_VARS']['BE']['fileadminDir'] = $this->tempSave['fileadminDir'];	
+		
+		unset ($GLOBALS['FILEMOUNTS']['__unittest']);
+	}
+	*/
+
+}
+
+?>
