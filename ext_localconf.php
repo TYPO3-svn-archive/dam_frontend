5c1ab3b33b459029698d660eba8cfcf1e81062f4
stefan.busemann <stefan.busemann@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-06-21 12:47:02 +0000

    #Feature 8214: flexform
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dam_frontend/trunk@34654 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -20,6 +20,8 @@ t3lib_extMgm::addTypoScript($_EXTKEY,'editorcfg','
 
 t3lib_extMgm::addPItoST43($_EXTKEY,'pi2/class.tx_damfrontend_pi2.php','_pi2','list_type',1);
 
+t3lib_extMgm::addPItoST43($_EXTKEY,'pi3/class.tx_damfrontend_pi3.php','_pi3','list_type',1);
+
 // including the eID for the pushfile
 $TYPO3_CONF_VARS['FE']['eID_include']['dam_frontend_push'] = t3lib_extMgm::extPath($_EXTKEY).'pushfile.php';
 ?>
--- ext_tables.php
+++ ext_tables.php
@@ -249,6 +249,12 @@ t3lib_div::loadTCA('tt_content');
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi2']='layout,select_key';
 t3lib_extMgm::addPlugin(array('LLL:EXT:dam_frontend/locallang_db.xml:tt_content.list_type_pi2', $_EXTKEY.'_pi2'),'list_type');
 t3lib_extMgm::addStaticFile($_EXTKEY,"pi2/static/","DAM Frontend Filelist");
+
+t3lib_extMgm::addPlugin(array('LLL:EXT:dam_frontend/locallang_db.xml:tt_content.list_type_pi3', $_EXTKEY.'_pi3'),'list_type');
+t3lib_extMgm::addStaticFile($_EXTKEY,"pi3/static/","DAM frontend basket case");
+t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi3', 'FILE:EXT:'.$_EXTKEY.'/pi3/flexform.xml');
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi3'] = 'pi_flexform';
+
 if (TYPO3_MODE=="BE") $TBE_MODULES_EXT["xMOD_db_new_content_el"]["addElClasses"]["tx_damfrontend_pi2_wizicon"] = t3lib_extMgm::extPath($_EXTKEY).'pi2/class.tx_damfrontend_pi2_wizicon.php';
 
 ?>
