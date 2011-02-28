e567cf377e3e0577adbbdfe430d1c4041c4c5d10
stefan.busemann <stefan.busemann@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-02-28 23:31:08 +0000

    support for new access model
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dam_frontend/trunk@44425 735d13b6-9817-0410-8766-e36946ffe9aa

--- DAL/class.tx_damfrontend_DAL_documents.php
+++ DAL/class.tx_damfrontend_DAL_documents.php
@@ -281,30 +281,37 @@ require_once(PATH_tslib.'class.tslib_content.php');
 				$catlist = $this->getCategoriesByDoc($docID);
 			}
 
-			// FIXME
-			// if relID ==1 check access field of cats instead
-
-			// accumulates all groups
 			$grouparray = array();
 			foreach ($catlist as $category)
 			{
 				if ($category['uid']>0) {
-					$mm_table = 'tx_dam_cat_'.$this->relations[$relID].'_mm';
-					// executing database search
-					$local_table = $this->catTable;
-					$foreign_table = 'fe_groups';
-					$where = 'AND '.$local_table.'.uid = '.$category['uid'];
-					$select = $foreign_table.'.*';
-					$res = $GLOBALS['TYPO3_DB']->exec_SELECT_mm_query($select,$local_table, $mm_table, $foreign_table, $where);
-
-					// adding groups from the database to the GroupArray - check if group is already in list
-					while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res))
-					{
-						if (!is_array($this->catLogic->findUidinList($grouparray, $row['uid'])))
-						{
-							$grouparray[] = $row;
+					if ($relID==1) {
+						$groups =  explode(',',$category['fe_group']);
+						foreach($groups as $group) {
+							if ($group)	{
+								$row = $GLOABLS['TYPO_3']->exec_SELECTgetSingleRow ('*', 'fe_groups', 'uid = ' . $group);
+								$grouparray[] = $row;
+							}
 						}
+					}
+					else {
+						$mm_table = 'tx_dam_cat_'.$this->relations[$relID].'_mm';
+						// executing database search
+						$local_table = $this->catTable;
+						$foreign_table = 'fe_groups';
+						$where = 'AND '.$local_table.'.uid = '.$category['uid'];
+						$select = $foreign_table.'.*';
+						$res = $GLOBALS['TYPO3_DB']->exec_SELECT_mm_query($select,$local_table, $mm_table, $foreign_table, $where);
 
+						// adding groups from the database to the GroupArray - check if group is already in list
+						while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res))
+						{
+							if (!is_array($this->catLogic->findUidinList($grouparray, $row['uid'])))
+							{
+								$grouparray[] = $row;
+							}
+
+						}
 					}
 				}
 			}
