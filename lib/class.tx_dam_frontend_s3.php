34382057480edfdb7b0b0098eea397c2ede6fce7
comprock <comprock@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-08-06 11:04:28 +0000

    add lib/
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dam_frontend/branches/s3@65196 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ lib/class.tx_dam_frontend_s3.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *  S3 DAM helper for file operations
+ *
+ *  @author Michael Cannon <michael@typo3vagabond.com>
+ */
+
+class tx_dam_frontend_s3 {
+	public function filePostTrigger( $action, $id = null ) {
+		t3lib_div::devLog( $action, __FUNCTION__, 0, false );
+		switch( $action ) {
+		case 'upload':
+			t3lib_div::devLog( true, __FUNCTION__, 0, $id );	
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	return true;
+}
+
+?>
