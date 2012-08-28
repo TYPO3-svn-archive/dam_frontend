b50e89452ffb4804b5ec367ce6d198f1779f7029
comprock <comprock@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-08-28 11:47:44 +0000

    FIXME notes
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dam_frontend/branches/s3@65669 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/class.tx_dam_frontend_s3.php
+++ lib/class.tx_dam_frontend_s3.php
@@ -7,10 +7,14 @@
 
 class tx_dam_frontend_s3 {
 	public function filePostTrigger( $action, $id = null ) {
+		// t3lib_div::devLog( $action, __FUNCTION__, 0, $id );	
 		$db						= $GLOBALS['TYPO3_DB'];
 
 		switch( $action ) {
 		case 'upload':
+			// FIXME doesn't work for changed file_names
+			// FIXME no versioning done
+
 			$target_file		= $id['target_file'];
 			$file_name			= basename( $target_file );
 			$file_path			= dirname( $target_file );
